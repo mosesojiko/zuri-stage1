0b6cb9c0a8a65d55ec3c3d27d67cad4c690aeadd
Ojiko Moses <mosganda2022@gmail.com>
2022-10-29 14:38:17 -0800

    Feat: Added image with twitter and slack usernames

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      <h1>My profile</h1>
+      {/* The images section*/}
+        <div>
+          <div>
+            <img id="profile_img" src="/images/moses1.jpg" alt="" />
+        </div>
+        <div>
+          <p id="twitter">Twitter: @OjikoMoses</p>
+          <p id="slack">Slack: mosganda</p>
+        </div>
+      </div>
+      
     </div>
   );
 }

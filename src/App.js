2edcd2ea59cbd601b5f40b2f4962d44eb289e1ae
Ojiko Moses <mosganda2022@gmail.com>
2022-10-29 16:12:54 -0800

    Feat: Added all links

--- src/App.js
+++ src/App.js
@@ -18,8 +18,40 @@ function App() {
       {/* The links section*/}
       <div>
         <div>
-      <a href='https://training.zuri.team' target="_blank">
-        <button>Zuri Team</button>
+      <a id="btn_zuri" href='https://training.zuri.team' target="_blank">
+        Zuri Team
+      </a>
+        </div>
+        <div>
+      <a id="books" href='http://books.zuri.team' target="_blank">
+            
+              <h3>Books</h3>
+              <p>This is where you find books about design and coding.</p>
+        
+      </a>
+        </div>
+        <div>
+      <a id="book_python" href='https://books.zuri.team/python-for-beginners?ref_id=mosganda' target="_blank">
+            
+              <h3>Python for beginners</h3>
+              <p>The best rated book for python development. Only two copies remaining.</p>
+       
+      </a>
+        </div>
+        <div>
+      <a id="pitch" href='https://background.zuri.team' target="_blank">
+            
+              <h3>Pitch</h3>
+              <p>My job is to run background checks on coders in less than 30 minutes.</p>
+        
+      </a>
+        </div>
+        <div>
+      <a id="book_design" href='https://books.zuri.team/design-rules' target="_blank">
+            
+              <div><h3>Design Rules</h3>
+              <p>Become a professional designer for free.</p></div>
+        
       </a>
     </div>
       </div>

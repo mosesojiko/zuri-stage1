7eb0ad158e1abf3a0cf8d3f7fa7456d9b392eeef
Ojiko Moses <mosganda2022@gmail.com>
2022-11-03 11:56:13 -0800

    feat: Created the contact page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home'
+import Home from './pages/Home';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path = '/' index element= {<Home />} />
+        <Route path='/' index element={<Home />} />
+        <Route path = '/contact' element = {<Contact />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Contact() {
+    return (
+        <div>This is the contact pages</div>
+    )
+}
+
+export default Contact
